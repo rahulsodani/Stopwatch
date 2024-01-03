ee14bbceced9d3acb8fe991f17a9e7d9c0b795ac
rahulsodani <62386165+rahulsodani@users.noreply.github.com>
2024-01-03 10:56:31 +0530

    Update stopwatch.js

--- stopwatch.js
+++ stopwatch.js
@@ -4,6 +4,7 @@
 // the last 2 digits in right side of stopwatch keeps track of millisconds in the multiples of 10 milliseconds
 var seconds = document.getElementById("seconds");
 var mseconds = document.getElementById("mseconds");
+var secondsvirtual=document.querySelector(".secondsvirtual");
 // this is to increase the counter by 1 in 10 milliseconds ==>so in 100 milliseconds means counter increases by 10 units and in 1sec by 100 units 
 var counter = 0;
 // id is used for stop the interval. Declared in global scope as used in multiple functions to stop the interval
@@ -15,17 +16,29 @@ function runStopwatch() {
     counter = counter + 1;
     ms = counter % 100;
     s = parseInt(counter / 100);
-    console.log(s);
+
     if (ms < 10) {
         mseconds.textContent = 0 + "" + ms;
     }
     else
         mseconds.textContent = ms;
-    if (s < 10) {
-        seconds.textContent = 0 + "" + s;
+    if(s!=parseInt(secondsvirtual.textContent))
+    {
+        console.log(s);
+        if (s < 10) {
+            secondsvirtual.textContent = 0 + "" + s;
+            
+        }
+        else
+            {
+                secondsvirtual.innerText = s;
+            }
+        secondsvirtual.classList.add("classtop");
+        setTimeout(() => {
+            secondsvirtual.classList.remove("classtop");
+            seconds.innerText=secondsvirtual.innerText;
+        },500);
     }
-    else
-        seconds.textContent = s;
 }
 // triggers when start button is clicked
 function startStopWatch() {
@@ -41,6 +54,7 @@ function restartStopWatch() {
     ms = s = counter = 0;
     mseconds.textContent = 0 + "" + 0;
     seconds.textContent = 0 + "" + 0;
+    secondsvirtual.innerText=0+""+0;
 }
 // handle all the click events
 function handleClickListener(event) {
